8957d0fb9c76f20a6da46cb13132301f56b0a598
juliusryan25 <63332117+juliusryan25@users.noreply.github.com>
2020-07-22 20:34:56 +0700

    upload tugas route

---
+++ Tugas route/views/portofolio.blade.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+  </head>
+  <body> 
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <!-- Image and text -->
+                <nav class="navbar navbar-light bg-trasprent">
+                <a class="navbar-brand" href="#">
+                    <img src="/images/hacker.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+                    Portofolio
+                </a>
+                </nav>
+            </div>
+            <div class="col-12" >
+            <div class="jumbotron" style="background-image:url(/images/bg.jpg);" >
+                
+                   <center><img src="/images/muka.jpg" style="width:150px;border-radius:70px" alt=""></center>
+                   <center> <b style="font-size:25px;font-family: 'Pacifico', cursive;">UX / UI Designer & Programmer</b></center>
+                
+                </div>
+            </div>
+           </div>
+           <div class="row">
+           <div class="col-4">
+              <div class="card">
+                <div class="card-body">
+                   <center><img src="/images/ui.png" alt="">
+                    <P>UI</P>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">80%</div>
+                    </div>
+                </center>
+                </div>
+              </div>
+           </div>
+           <div class="col-4">
+              <div class="card">
+                <div class="card-body">
+                   <center><img src="/images/ux.png" alt="">
+                    <P>UX</P>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%">70%</div>
+                    </div>
+                </center>
+                </div>
+              </div>
+           </div>
+           <div class="col-4">
+              <div class="card">
+                <div class="card-body">
+                   <center><img src="/images/code.png" alt="">
+                    <P>Programmer</P>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 90%">90%</div>
+                    </div>
+                </center>
+                </div>
+              </div>
+           </div>
+           </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ Tugas route/views/profile.blade.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Gayathri:wght@700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="card mt0 mb-0">
+            <div class="card-body" style="background:linear-gradient(to right, white ,DodgerBlue)">
+
+
+                <div class="row">
+
+                    <div class="col-12">
+                        <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+                            <ol class="carousel-indicators">
+                                <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                                <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                                <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                            </ol>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="/images/Capture.png" style="border-radius: 10px;" class="d-block w-100"
+                                        alt="...">
+                                    <div class="carousel-caption d-none d-md-block">
+                                        <h5>1</h5>
+                                        <p>Bersekolah di SMK TARUNA BHAKTI</p>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="/images/rpl2.png" style="border-radius: 10px;" class="d-block w-100"
+                                        alt="...">
+                                    <div class="carousel-caption d-none d-md-block">
+                                        <h5>2</h5>
+                                        <p>Jurusan Rekayasa Perangkat Lunak</p>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="/images/muka.png" style="border-radius: 10px;" class="d-block w-100"
+                                        alt="...">
+                                    <div class="carousel-caption d-none d-md-block">
+                                        <h5>3</h5>
+                                        <p>My Face</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button"
+                                data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button"
+                                data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-12 mt-3">
+                        <div class="card mb-3" style="max-width: 100%;">
+                            <div class="row no-gutters">
+                                <div class="col-md-4">
+                                    <center>
+                                        <img src="/images/hacker.png" style="width:50%" class="card-img mt-4" alt="...">
+                                    </center>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Julius Ryan Listianto</h5>
+                                        <p style="font-family: 'Gayathri', sans-serif;" class="card-text ">Biasa dipanggil Julius / Ryan ,Umur 17 tahun sekolah di SMK
+                                            TARUNA BHAKTI Jurusan Rekayasa Perangkat Lunak (Softwere Engineer).
+                                            Saya lahir di Depok , 25 September 2003 . Sejak kecil saya suka dengan
+                                            teknologi oleh karena itu saya termotivasi untuk mempelajari sesuatu yang
+                                            berkaitan dengan
+                                            teknologi yaitu menjadi seorang programmer seperti yang saya pelajari saat
+                                            ini </p>
+                                            <p><img src="/images/whatsapp.png" alt=""> &nbsp 088224920697 &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <img src="/images/youtube.png" alt=""> &nbsp Julius Ryan</p>
+                                            <p><img src="/images/instagram.png" alt=""> &nbsp juliusryan25_ &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <img src="/images/gmail.png" alt=""> &nbsp ryanjulius742@gmail.com</p>
+                                            <p></p>
+                                        <p class="card-text"><small class="text-muted">copyright by Julius </small></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
