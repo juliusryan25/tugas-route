8abd7d422b8c6f85e3ed8b2be9eabb1541472f89
juliusryan25 <63332117+juliusryan25@users.noreply.github.com>
2020-07-23 11:36:11 +0700

    Update experience.blade.php
    update experience

--- Tugas route/views/experience.blade.php
+++ Tugas route/views/experience.blade.php
@@ -9,152 +9,170 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
         integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-        <link href="https://fonts.googleapis.com/css2?family=Berkshire+Swash&display=swap" rel="stylesheet">
-    
+    <link href="https://fonts.googleapis.com/css2?family=Berkshire+Swash&display=swap" rel="stylesheet">
+
 </head>
 
 <body>
     <div class="container-fluid">
-      <div class="row">
-        <div class="col-12 mt-3">
-      <nav class="navbar navbar-light bg-white" style="font-family: 'Berkshire Swash', cursive;">
-          <h2>Experience</h2>
-      </nav>
-      </div>
-    <div class="col-12 mt-3">
-        <div class="card mb-3" style="max-width: 100%;background: linear-gradient(to right , white ,white,DodgerBlue);font-family: 'Playfair Display', serif;">
-            <div class="row no-gutters">
-                <div class="col-md-4">
-                    <center>
-                    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                <div class="carousel-inner">
-                  <div class="carousel-item active">
-                  <img src="/images/seo.jpg" style="width:200px" class="card-img" alt="...">
-                  </div>
-                  <div class="carousel-item">
-                    <img src="/images/seo2.png" style="width:332px" class="card-img" alt="...">
-                  </div>
-                </div>
-                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Next</span>
-                </a>
-              </div>  
-                    </center>
-                </div>
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <p style="font-size: 40px;" class="card-title">Workshop SEO
-                            <img src="/images/search.png" alt="" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                       </p>
+        <div class="row">
+            <div class="col-12 mt-3">
+                <nav class="navbar navbar-light bg-white"
+                    style="font-family: 'Berkshire Swash', cursive;background-image:url(/images/bg.jpg);">
+                    <h2>Experience</h2>
+                </nav>
+            </div>
+            <div class="col-12 mt-3">
+                <div class="card mb-3"
+                    style="max-width: 100%;background: linear-gradient(to right , white ,white,DodgerBlue);font-family: 'Playfair Display', serif;">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <center>
+                                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                                    <div class="carousel-inner">
+                                        <div class="carousel-item active">
+                                            <img src="/images/seo.jpg" style="width:200px" class="card-img" alt="...">
+                                        </div>
+                                        <div class="carousel-item">
+                                            <img src="/images/seo2.png" style="width:332px" class="card-img" alt="...">
+                                        </div>
+                                    </div>
+                                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button"
+                                        data-slide="prev">
+                                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                        <span class="sr-only">Previous</span>
+                                    </a>
+                                    <a class="carousel-control-next" href="#carouselExampleControls" role="button"
+                                        data-slide="next">
+                                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                        <span class="sr-only">Next</span>
+                                    </a>
+                                </div>
+                            </center>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <p style="font-size: 40px;" class="card-title">Workshop SEO
+                                    <img src="/images/search.png" alt="" style="cursor:pointer" data-toggle="collapse"
+                                        data-target="#collapseExample" aria-expanded="false"
+                                        aria-controls="collapseExample">
+                                </p>
 
-                        <div class="collapse" id="collapseExample">
-                            <div class="card card-body">
-                              SEO DEVELOPMENT "Lets Talk About SEO" 7 September 2019 <br> Pemateri : <br>
-                              Renaldi (Android Developer kwikku.com)<br>
-                              Andy Chrisman Sianturi (Digital Marketing Specialist Toffedev)
-                               
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    
-    <div class="col-12 mt-3">
-        <div class="card mb-3" style="max-width: 100%;background: linear-gradient(to right , white ,white,DodgerBlue);font-family: 'Playfair Display', serif;">
-            <div class="row no-gutters">
-                <div class="col-md-4">
-                    <center>
-                    <div id="carouselExampleControls1" class="carousel slide" data-ride="carousel">
-                <div class="carousel-inner">
-                  <div class="carousel-item active">
-                  <img src="/images/sada1.jpg" style="width:250px" class="card-img" alt="...">
-                  </div>
-                  <div class="carousel-item">
-                    <img src="/images/sada2.png" style="width:188px" class="card-img" alt="...">
-                  </div>
-                </div>
-                <a class="carousel-control-prev" href="#carouselExampleControls1" role="button" data-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleControls1" role="button" data-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Next</span>
-                </a>
-              </div>  
-                    </center>
-                </div>
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <p style="font-size: 40px;" class="card-title">Kunjungan Industri
-                        <img src="/images/search.png" alt="" data-toggle="collapse" data-target="#collapseExample1" aria-expanded="false" aria-controls="collapseExample">
-                          </p>
-                        <div class="collapse" id="collapseExample1">
-                            <div class="card card-body">
-                                  Kunjungan Industri PT. Sada Technologi Jl. Raden Inten II No.9, RT.8/RW.10, Duren Sawit, Kec. Duren Sawit, Kota Jakarta Timur, Daerah Khusus Ibukota Jakarta 
-                               
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-</div>
+                                <div class="collapse" id="collapseExample">
+                                    <div class="card card-body">
+                                        SEO DEVELOPMENT "Lets Talk About SEO" 7 September 2019 <br> Pemateri : <br>
+                                        Renaldi (Android Developer kwikku.com)<br>
+                                        Andy Chrisman Sianturi (Digital Marketing Specialist Toffedev)
 
-        <div class="col-12 mt-3">
-        <div class="card mb-3" style="max-width: 100%;background: linear-gradient(to right , white ,white,DodgerBlue);font-family: 'Playfair Display', serif;">
-            <div class="row no-gutters">
-                <div class="col-md-4">
-                    <center>
-                    <div id="carouselExampleControls2" class="carousel slide" data-ride="carousel">
-                <div class="carousel-inner">
-                  <div class="carousel-item active">
-                  <img src="/images/kks.jpg" style="width:320px" class="card-img" alt="...">
-                  </div>
-                  <div class="carousel-item">
-                    <img src="/images/big1.jpg" style="width:240px" class="card-img" alt="...">
-                  </div>
-                  <div class="carousel-item">
-                    <img src="/images/big2.jpg" style="width:240px" class="card-img" alt="...">
-                  </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <a class="carousel-control-prev" href="#carouselExampleControls2" role="button" data-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleControls2" role="button" data-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Next</span>
-                </a>
-              </div>  
-                    </center>
+            </div>
+
+            <div class="col-12 mt-3">
+                <div class="card mb-3"
+                    style="max-width: 100%;background: linear-gradient(to right , white ,white,DeepPink);font-family: 'Playfair Display', serif;">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <center>
+                                <div id="carouselExampleControls1" class="carousel slide" data-ride="carousel">
+                                    <div class="carousel-inner">
+                                        <div class="carousel-item active">
+                                            <img src="/images/sada1.jpg" style="width:250px" class="card-img" alt="...">
+                                        </div>
+                                        <div class="carousel-item">
+                                            <img src="/images/sada2.png" style="width:188px" class="card-img" alt="...">
+                                        </div>
+                                    </div>
+                                    <a class="carousel-control-prev" href="#carouselExampleControls1" role="button"
+                                        data-slide="prev">
+                                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                        <span class="sr-only">Previous</span>
+                                    </a>
+                                    <a class="carousel-control-next" href="#carouselExampleControls1" role="button"
+                                        data-slide="next">
+                                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                        <span class="sr-only">Next</span>
+                                    </a>
+                                </div>
+                            </center>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <p style="font-size: 40px;" class="card-title">Kunjungan Industri
+                                    <img src="/images/search.png" alt="" style="cursor:pointer" data-toggle="collapse"
+                                        data-target="#collapseExample1" aria-expanded="false"
+                                        aria-controls="collapseExample">
+                                </p>
+                                <div class="collapse" id="collapseExample1">
+                                    <div class="card card-body">
+                                        Kunjungan Industri PT. Sada Technologi Jl. Raden Inten II No.9, RT.8/RW.10,
+                                        Duren Sawit, Kec. Duren Sawit, Kota Jakarta Timur, Daerah Khusus Ibukota Jakarta
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <p style="font-size: 40px;" class="card-title">KKS 2020
-                        <img src="/images/search.png" alt="" data-toggle="collapse" data-target="#collapseExample2" aria-expanded="false" aria-controls="collapseExample">
-                        </p>
-                        <div class="collapse" id="collapseExample2">
-                            <div class="card card-body">
-                                  Kamp Kreatif Sekolah 2020 <br>
-                                  Materi disampaikan melalui daring secara online dengan meeting maupun google classroom.<br>
-                                  Siswa dibagi menjadi beberapa kelompok , saya masuk dalam kelompok Big Data
-                               
+            </div>
+
+            <div class="col-12 mt-3">
+                <div class="card mb-3"
+                    style="max-width: 100%;background: linear-gradient(to right , white ,white,OrangeRed);font-family: 'Playfair Display', serif;">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <center>
+                                <div id="carouselExampleControls2" class="carousel slide" data-ride="carousel">
+                                    <div class="carousel-inner">
+                                        <div class="carousel-item active">
+                                            <img src="/images/kks.jpg" style="width:320px" class="card-img" alt="...">
+                                        </div>
+                                        <div class="carousel-item">
+                                            <img src="/images/big1.jpg" style="width:240px" class="card-img" alt="...">
+                                        </div>
+                                        <div class="carousel-item">
+                                            <img src="/images/big2.jpg" style="width:240px" class="card-img" alt="...">
+                                        </div>
+                                    </div>
+                                    <a class="carousel-control-prev" href="#carouselExampleControls2" role="button"
+                                        data-slide="prev">
+                                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                        <span class="sr-only">Previous</span>
+                                    </a>
+                                    <a class="carousel-control-next" href="#carouselExampleControls2" role="button"
+                                        data-slide="next">
+                                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                        <span class="sr-only">Next</span>
+                                    </a>
+                                </div>
+                            </center>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <p style="font-size: 40px;" class="card-title">KKS 2020
+                                    <img src="/images/search.png" alt="" style="cursor:pointer" data-toggle="collapse"
+                                        data-target="#collapseExample2" aria-expanded="false"
+                                        aria-controls="collapseExample">
+                                </p>
+                                <div class="collapse" id="collapseExample2">
+                                    <div class="card card-body">
+                                        Kamp Kreatif Sekolah 2020 <br>
+                                        Materi disampaikan melalui daring secara online dengan meeting maupun google
+                                        classroom.<br>
+                                        Siswa dibagi menjadi beberapa kelompok , saya masuk dalam kelompok Big Data
+
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    </div>
     </div>
     </div>
     <!-- Optional JavaScript -->
