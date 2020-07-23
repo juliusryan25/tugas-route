d514efcdf13b55001a74b49ae46ad1bb2e14e410
juliusryan25 <63332117+juliusryan25@users.noreply.github.com>
2020-07-23 11:38:25 +0700

    Update portofolio.blade.php
    update portofolio

--- Tugas route/views/portofolio.blade.php
+++ Tugas route/views/portofolio.blade.php
@@ -1,80 +1,190 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
-  </head>
-  <body> 
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500&display=swap" rel="stylesheet">
+</head>
+
+<body>
     <div class="container-fluid">
         <div class="row">
             <div class="col-12">
                 <!-- Image and text -->
                 <nav class="navbar navbar-light bg-trasprent">
-                <a class="navbar-brand" href="#">
-                    <img src="/images/hacker.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
-                    Portofolio
-                </a>
+                    <a class="navbar-brand" href="#">
+                        <img src="/images/hacker.png" width="30" height="30" class="d-inline-block align-top" alt=""
+                            loading="lazy">
+                        Portofolio
+                    </a>
                 </nav>
             </div>
-            <div class="col-12" >
-            <div class="jumbotron" style="background-image:url(/images/bg.jpg);" >
-                
-                   <center><img src="/images/muka.jpg" style="width:150px;border-radius:70px" alt=""></center>
-                   <center> <b style="font-size:25px;font-family: 'Pacifico', cursive;">UX / UI Designer & Programmer</b></center>
-                
+            <div class="col-12">
+                <div class="jumbotron" style="background-image:url(/images/bg.jpg);">
+
+                    <center><img src="/images/muka.jpg" style="width:150px;border-radius:70px" alt=""></center>
+                    <center> <b style="font-size:25px;font-family: 'Pacifico', cursive;">UX / UI Designer &
+                            Programmer</b></center>
+
                 </div>
             </div>
-           </div>
-           <div class="row">
-           <div class="col-4">
-              <div class="card">
-                <div class="card-body">
-                   <center><img src="/images/ui.png" alt="">
-                    <P>UI</P>
-                    <div class="progress">
-                        <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">80%</div>
-                    </div>
-                </center>
-                </div>
-              </div>
-           </div>
-           <div class="col-4">
-              <div class="card">
-                <div class="card-body">
-                   <center><img src="/images/ux.png" alt="">
-                    <P>UX</P>
-                    <div class="progress">
-                        <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%">70%</div>
-                    </div>
-                </center>
-                </div>
-              </div>
-           </div>
-           <div class="col-4">
-              <div class="card">
-                <div class="card-body">
-                   <center><img src="/images/code.png" alt="">
-                    <P>Programmer</P>
-                    <div class="progress">
-                        <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 90%">90%</div>
-                    </div>
-                </center>
-                </div>
-              </div>
-           </div>
-           </div>
         </div>
+        <div class="row">
+            <div class="col-4">
+                <div class="card">
+                    <div class="card-body">
+                        <center><img src="/images/ui.png" alt="">
+                            <P style="font-family: 'Dancing Script', cursive;">UI</P>
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar"
+                                    aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">80%
+                                </div>
+                            </div>
+                        </center>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card">
+                    <div class="card-body">
+                        <center><img src="/images/ux.png" alt="">
+                            <P style="font-family: 'Dancing Script', cursive;">UX</P>
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar"
+                                    aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%">70%
+                                </div>
+                            </div>
+                        </center>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card">
+                    <div class="card-body">
+                        <center><img src="/images/code.png" alt="">
+                            <P style="font-family: 'Dancing Script', cursive;">Programmer</P>
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar"
+                                    aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 90%">90%
+                                </div>
+                            </div>
+                        </center>
+                    </div>
+                </div>
+            </div>
+            <center>
+                <div class="col-9">
+                    <div class="card mt-4 " style="border:0">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-12">
+                                    <center>
+                                        <h1 style="font-family: 'Dancing Script', cursive;">Creation</h1>
+                                    </center>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-4">
+                                    <div class="card"
+                                        style="width: 18rem;background: linear-gradient(to bottom ,white , white ,white,OrangeRed);">
+                                        <img src="/images/stmart.jpg" class="card-img-top"
+                                            style="padding:5px;border-radius:8px;height:165px" alt="...">
+                                        <div class="card-body">
+                                            <p class="card-text">Starbhak Mart</p>
+                                            <center>
+                                                <p><img src="/images/search.png" alt="" style="cursor:pointer"
+                                                        data-toggle="collapse" data-target="#collapseExample"
+                                                        aria-expanded="false" aria-controls="collapseExample">
+                                            </center>
+                                            </p>
+                                            <div class="collapse" id="collapseExample">
+                                                <div class="card card-body">
+                                                    Starbhak Mart adalah sebuah aplikasi berbasis web yang berfungsi
+                                                    mencatat detail pesanan berupa menu dan jumlah dipesan
+                                                    serta mengelola administrasi pembayaran beserta dengan pajaknya .
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-4">
+                                    <div class="card"
+                                        style="width: 18rem;background: linear-gradient(to bottom ,white , white ,white,DeepPink);">
+                                        <img src="/images/lon.jpg" class="card-img-top"
+                                            style="padding:5px;border-radius:8px;height:165px" alt="...">
+                                        <div class="card-body">
+                                            <p class="card-text">Starbhak Lundry</p>
+                                            <center>
+                                                <p><img src="/images/search.png" alt="" style="cursor:pointer"
+                                                        data-toggle="collapse" data-target="#collapseExample1"
+                                                        aria-expanded="false" aria-controls="collapseExample">
+                                            </center>
+                                            </p>
+                                            <div class="collapse" id="collapseExample1">
+                                                <div class="card card-body">
+                                                    Starbhak Laundry adalah sebuah aplikasi desktop yang berfungsi untuk
+                                                    administrasi , seperti rekap data karyawan
+                                                    , rekap pesanan dan mengeloah pembayaran dalam bentuk struk
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-4">
+                                    <div class="card"
+                                        style="width: 100%;background: linear-gradient(to bottom ,white , white ,white,DodgerBlue);">
+                                        <img src="/images/piket.jpg" class="card-img-top"
+                                            style="padding:5px;border-radius:8px;height:165px" alt="...">
+                                        <div class="card-body">
+                                            <p class="card-text">Aplikasi Manajemen Piket Kelas</p>
+                                            <center>
+                                                <p><img src="/images/search.png" alt="" style="cursor:pointer"
+                                                        data-toggle="collapse" data-target="#collapseExample2"
+                                                        aria-expanded="false" aria-controls="collapseExample">
+                                            </center>
+                                            </p>
+                                            <div class="collapse" id="collapseExample2">
+                                                <div class="card card-body">
+                                                    Manajemen Piket Kelas merupakan aplikasi administrasi piket Kelas
+                                                    fungsi diantaranya adalah melihat daftar piket ,
+                                                    input dan hapus data siswa , dan menilai kinerja siswa dalam
+                                                    melaksanakan piket.
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </div>
+    </div>
+    </center>
+    </div>
     </div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-  </body>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+    </script>
+</body>
+
 </html>
